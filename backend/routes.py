8d4c143184bbb0d81b50577e949d3bc99bd1c506
LuuuFan <angelia.fan@gmail.com>
2018-08-20 14:49:45 -0700

    finish basic yelp api call

--- backend/routes.py
+++ backend/routes.py
@@ -1,9 +1,33 @@
 from backend import app
+from backend.key import YELP_API_KEY
+from pdb import set_trace as bp
+# uncomplete library
+# from yelp.client import Client
+from flask import request
+import json
+import requests
 
-@app.route('/opentable')
+
+@app.route('/api/opentable')
 def opentable():
 	return ''
 
-@app.route('/yelp')
+@app.route('/api/yelp')
 def yelp():
-	pass
+	# initialize client
+	# client = Client(YELP_API_KEY)
+	# business_response = client.business.get_by_id('yelp-san-francisco')
+	# print business_response.json()
+	# response = client.business.get_by_location('94404')
+	api_url_base = 'https://api.yelp.com/v3/businesses/search?term=restaurant&'
+	location = request.args.get('location')
+	url = api_url_base + 'location=' + location
+	headers = {
+		'Content-Type': 'application/json',
+		'Authorization': 'Bearer %s' % YELP_API_KEY,
+	}
+	response = requests.get(url, headers=headers)
+	if response.status_code == 200:
+		return response.content, 200
+	else:
+		return {'error': 'Cannot get restaurant information, try again later'}
--- key.js
+++ key.js
@@ -1,2 +1,3 @@
-yelp = '';
+YELP_API_KEY = '7AzKetI9iUF-ZdqNIap3z9d4VfqUYmpneo5BlqrukldMYwGIIckvirm-jTcF59M2HMH7GnVcbB8e7e9om6-OuxN2ZrIgabRNjNng6viVPS14AAZPFV0M9B20pglqWnYx';
+YELP_CLIENT_ID = 'R1Ms2oQu-rTEykdSrM2llQ';
 opentable = '';
